5e529bf1f386c7ce5350cb65430f7bb3992b1c60
Thomas Holmes <shadox@gmail.com>
2018-12-01 00:49:58 -0500

    Run both part 1 and 2 solutions.

--- pkg/p01/solution.go
+++ pkg/p01/solution.go
@@ -13,10 +13,16 @@ import (
 )
 
 func Run() {
+	p1()
+	p2()
+}
+
+func p1() {
 	f, err := os.Open(filepath.Join("data", "1-2.txt"))
 	if err != nil {
 		log.Panicln("Failed to open file", err)
 	}
+	defer f.Close()
 
 	bs, err := ioutil.ReadAll(f)
 	if err != nil {
@@ -41,19 +47,58 @@ func Run() {
 			log.Panicln("Could not convert string", string(lBytes), "to int", err)
 		}
 
-		log.Println("Number:", i)
+		numbers = append(numbers, i)
+	}
+
+	var acc int
+	for _, i := range numbers {
+		acc += i
+	}
+
+	log.Println("P1: Computed total freq of", acc)
+}
+
+func p2() {
+	f, err := os.Open(filepath.Join("data", "1-2.txt"))
+	if err != nil {
+		log.Panicln("Failed to open file", err)
+	}
+	defer f.Close()
+
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Panicln("Failed to read file", err)
+	}
+
+	buf := bufio.NewReader(bytes.NewBuffer(bs))
+
+	var numbers []int
+	for {
+		lBytes, err := buf.ReadBytes('\n')
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			log.Panicln("Failed to read line", err)
+		}
+
+		i, err := strconv.Atoi(strings.Trim(string(lBytes), "\r\n"))
+		if err != nil {
+			log.Panicln("Could not convert string", string(lBytes), "to int", err)
+		}
+
 		numbers = append(numbers, i)
 	}
 
 	result := scan(0, numbers, make(map[int]struct{}))
 
-	log.Println("Computed total freq of", result)
+	log.Println("P2: Computed total freq of", result)
 }
 
 func scan(acc int, numbers []int, seen map[int]struct{}) int {
 	for _, i := range numbers {
 		acc += i
-		log.Println("acc", acc)
 		if _, ok := seen[acc]; ok {
 			log.Println("Found duplicate frequency", acc)
 			return acc
